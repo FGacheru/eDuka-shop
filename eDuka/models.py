d0d2f809b764eb1440347d05afa0774e16d4440f
FGacheru <francisgacheru2001@gmail.com>
2021-02-01 15:27:28 +0300

    add Customer class with user,name and email

--- eDuka/models.py
+++ eDuka/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
