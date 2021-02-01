8fa5b749bdd1500543c3617c9d82fdd205bc1f78
FGacheru <francisgacheru2001@gmail.com>
2021-02-01 14:02:35 +0300

    add base template with static and bootstrap load

--- eDuka/views.py
+++ eDuka/views.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'all-store/index.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'all-store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'all-store/checkout.html', context)
