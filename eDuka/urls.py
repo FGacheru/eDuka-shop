ddf24552ce3ab089b66197a350ce7b5de78b2235
FGacheru <francisgacheru2001@gmail.com>
2021-02-02 18:12:20 +0300

    change url to path and import path

--- eDuka/urls.py
+++ eDuka/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.conf.urls import path,include
+from django.urls import path, include 
 from . import views
-
 urlpatterns = [
-    path('', views.index, name = "index"),
-    path('cart/', views.cart, name = "cart"),
-    path('checkout/', views.checkout, name = "checkout"),
-    
+    path('',views.index,name='index'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/',views.checkout,name ='checkout'),
+          
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
