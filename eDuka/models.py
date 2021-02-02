cdb912ee1b1bdffef0f630eb78c8cc6e3af44f88
FGacheru <francisgacheru2001@gmail.com>
2021-02-02 10:55:11 +0300

    add class order with the transaction

--- eDuka/models.py
+++ eDuka/models.py
@@ -13,9 +13,20 @@ class Customer(models.Model):
 		return self.name
 
 class Product(models.Model):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
-
-    def __str__(self):
+	#image
+	def __str__(self):
 		return self.name
+
+class Order(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False, null=True,blank=False)
+	transaction_id = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return str(self.id)
+
+# 
