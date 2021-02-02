cee854f8551d08250ae6597ce30e0649bf37677c
FGacheru <francisgacheru2001@gmail.com>
2021-02-02 18:11:40 +0300

    add a property imageURL in the Product model

--- eDuka/models.py
+++ eDuka/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Customer(models.Model):
@@ -10,15 +11,25 @@ class Customer(models.Model):
 	email = models.CharField(max_length=200)
 
 	def __str__(self):
-		return self.name
+			return self.name
 
 class Product(models.Model):
-	name = models.CharField(max_length=200, null=True)
+	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
-	#image
+	image = models.ImageField(null=True,blank=True)
+
 	def __str__(self):
-		return self.name
+			return self.name
+
+	@property
+	def imageURL(self):
+		try:
+				url = self.image.url
+		except:
+				url = ''
+		return url
+
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
@@ -27,10 +38,24 @@ class Order(models.Model):
 	transaction_id = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
-		return str(self.id)
+			return str(self.id)
 
 class Item(models.Model):
 	Product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+
+class DeliveryAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	state = models.CharField(max_length=200, null=False)
+	zipcode = models.CharField(max_length=200, null=False)
+	date_added = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+			return self.address
+
