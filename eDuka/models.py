46d64c092e0e227d99746271ab088706cb9e9dc7
FGacheru <francisgacheru2001@gmail.com>
2021-02-02 21:10:22 +0300

    authenticate the checkout for the user to be able to checkout if they are log in and add total items and amount in the checkout template

--- eDuka/models.py
+++ eDuka/models.py
@@ -40,6 +40,18 @@ class Order(models.Model):
 	def __str__(self):
 			return str(self.id)
 
+	@property
+	def get_cart_total(self):
+		items = self.item_set.all()
+		total = sum([item.get_total for item in items])
+		return total
+
+	@property
+	def get_cart_items(self):
+		items = self.item_set.all()
+		total = sum([item.quantity for item in items])
+		return total
+
 class Item(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
--- eDuka/views.py
+++ eDuka/views.py
@@ -19,11 +19,23 @@ def cart(request):
         items = order.item_set.all()
 
     else:
+        #Create Empty cart for now for none-logged in users
         items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
 
-    context = {'items':items}
+    context = {'items':items, 'order':order}
     return render(request, 'all-store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.item_set.all()
+
+    else:
+        #Create Empty cart for now for none-logged in users
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'all-store/checkout.html', context)
