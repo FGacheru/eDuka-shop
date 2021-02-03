7fb232d2f96b37b138668bb8bba9f09121e02648
FGacheru <francisgacheru2001@gmail.com>
2021-02-03 10:46:40 +0300

    add comment class and import timzone

--- eDuka/models.py
+++ eDuka/models.py
@@ -91,3 +91,9 @@ class Post(models.Model):
     def number_of_comments(self):
         return Comment.objects.filter(post_connected=self).count()
 
+class Comment(models.Model):
+    content = models.TextField(max_length=150)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_connected = models.ForeignKey(Post, on_delete=models.CASCADE)
+   
